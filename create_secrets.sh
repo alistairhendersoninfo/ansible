1a189c7eaf55932c02bafeb8d785457f0aa3d4e3
alistairhendersoninfo <al@alistairhenderson.info>
2025-02-07 12:58:39 +0000

    Update create_secrets.sh

--- create_secrets.sh
+++ create_secrets.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-cd /opt/basic_playbook
-
-
 # Define the group where secrets should be stored (Modify as needed)
 GROUP="all"  # Change this to webservers, dbservers, etc.
 SECRETS_FILE="group_vars/$GROUP/secrets.yml"
